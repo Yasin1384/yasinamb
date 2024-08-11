45ca30ab9562ecc54bf0a5795d3fd50929ad09b9
Yasin1384 <yasinamb.1384amb.11228@gmail.com>
2024-08-11 15:43:32 -0700

    scroll

--- Assets/Script/SingleTon.cs
+++ Assets/Script/SingleTon.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23f8baf745c9d32aa54bf7341907c7791488488616f3cff1201402b00f945ef4
-size 1992
+oid sha256:b10fab253ca296475c24972700db876ebad1ff0af70cc75f76bf917f3b8651b1
+size 2079
--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a002ad4f121e4214257406f821a40427918aaa80941598ef8a91214aba56b5c
-size 317
+oid sha256:21d47c442c079ccb10fef0b59e7e3d9104a7528ff94e7805e8a27c4d5b66909e
+size 288
