6974fa41f45a68a9bc21c62981bd865b7b24038c
Yasin1384 <yasinamb.1384amb.11228@gmail.com>
2024-08-12 13:57:57 -0700

    Stop Audio

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf08aa10d923b1216786c74959721ec868aa58a0d749563259d58175f365eb8c
-size 386
+oid sha256:065003ae0c722ee80cc0f4c9d14666f8ab139b3de7f4db3d5edd0f9e61d1727a
+size 461
