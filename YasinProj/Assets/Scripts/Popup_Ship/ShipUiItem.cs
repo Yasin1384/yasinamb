b4744910bb388434ee10cee6269140f0db0da766
Yasin1384 <yasinamb.1384amb.11228@gmail.com>
2024-08-22 11:59:43 -0700

    merge

--- YasinProj/Assets/Scripts/Popup_Ship/ShipUiItem.cs
+++ YasinProj/Assets/Scripts/Popup_Ship/ShipUiItem.cs
@@ -1,10 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
-
-public class ShipUiItem : MonoBehaviour
-{
-    [SerializeField] private Image img;
-}
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:ba66778fd9f377e9c0306b19876f07f9ce09203ad3ccb645808713247b77fb0d
+size 422
